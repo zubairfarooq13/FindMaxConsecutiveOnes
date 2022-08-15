536ce1007fe4beb86487f98cf8d676d40aef1dee
Zubair Farooq <43909324+zubairfarooq13@users.noreply.github.com>
2022-08-15 17:23:53 +0500

    Add project files.

---
+++ FindMaxConsecutiveOnes/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] nums = { 0, 1, 1,0,1,0,1,1,1 };
+int TotalOnes = 0;
+List<int> Consec = new List<int>();
+
+for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == 1)
+        {
+            TotalOnes++;
+        }
+        if (nums[i] == 0 && i != (nums.Length - 1))
+        {
+        Consec.Add(TotalOnes);
+            TotalOnes = 0;
+        }
+        if(i == (nums.Length - 1))
+        {
+            Consec.Add(TotalOnes);
+        }
+   }
+Console.WriteLine(Consec.Max());
